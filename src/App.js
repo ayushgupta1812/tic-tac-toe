58df7fe7fc14d7403e33c36924d3c06a55633cd0
ayushgupta1812 <107362286+ayushgupta1812@users.noreply.github.com>
2023-04-04 11:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let cells = ['', '', '', '', '', '', '', '', ''];
+  let currentPlayer = 'X';
+  let result = document.querySelector('.result');
+  let btns = document.querySelectorAll('.btn');
+  let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const ticTacToe = (element, index) => {
+    element.value = currentPlayer;
+    element.disabled = true;
+    cells[index] = currentPlayer;
+    currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+    result.innerHTML = `Player ${currentPlayer} Turn`;
+
+    for (let i = 0; i < conditions.length; i++) {
+      let condition = conditions[i];
+      let a = cells[condition[0]];
+      let b = cells[condition[1]];
+      let c = cells[condition[2]];
+
+      if (a == '' || b == '' || c == '') {
+        continue;
+      }
+
+      if ((a == b) && (b == c)) {
+        result.innerHTML = `Player ${a} Won 🎉`;
+        btns.forEach((btn) => btn.disabled = true);
+      }
+    }
+  };
+
+  function reset() {
+    cells = ['', '', '', '', '', '', '', '', ''];
+    btns.forEach((btn) => {
+      btn.value = '';
+    });
+    currentPlayer = 'X';
+    result.innerHTML = `Player X Turn`;
+    btns.forEach((btn) => btn.disabled = false);
+  };
+
+  document.querySelector('#reset').addEventListener('click', reset);
+
+  btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => ticTacToe(btn, i));
+  });
 }
 
 export default App;
